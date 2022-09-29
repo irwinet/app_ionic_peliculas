d03018fef45a47f4ff78c51c5c37905baa172729
Irwin Estrada <ext_iestrada@tottus.com.pe>
2022-09-29 11:57:09 -0500

    Refactor services - urlsBases

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '24eea1a4dd8a4b6122aade032afb97fb',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
